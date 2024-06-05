e929a812c361bb04d5dac1d93ce21689ca195ef4
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-06-05 15:05:40 +0700

    81 model customer

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -23,7 +23,38 @@ let postUsersApi = async (req, res) => {
     data: results,
   });
 };
+let updateUsersApi = async (req, res) => {
+  let id = req.body.id;
+  let name = req.body.name;
+  let email = req.body.email;
+  let city = req.body.city;
+
+  // await CURDService.updateUser(id, email, name, city);
+  let results = await User.updateOne(
+    { _id: id },
+    {
+      name,
+      email,
+      city,
+    }
+  );
+  return res.status(200).json({
+    errCode: 0,
+    data: results,
+  });
+};
+let deleteUsersApi = async (req, res) => {
+  let id = req.body.id;
+
+  let results = await User.deleteOne({ _id: id });
+  return res.status(200).json({
+    errCode: 0,
+    data: results,
+  });
+};
 module.exports = {
   getUsersApi,
   postUsersApi,
+  updateUsersApi,
+  deleteUsersApi,
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const routerAPI = express.Router();
-const { getUsersApi, postUsersApi } = require("../controllers/ApiController");
+const {
+  getUsersApi,
+  postUsersApi,
+  updateUsersApi,
+  deleteUsersApi,
+} = require("../controllers/ApiController");
 
 routerAPI.get("/users", getUsersApi);
 routerAPI.post("/users", postUsersApi);
+routerAPI.put("/users", updateUsersApi);
+routerAPI.delete("/users", deleteUsersApi);
+
 module.exports = routerAPI;
