58011f06fbae9ed71c89238a7bea54e44f7818fc
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-06-08 22:47:21 +0700

    115 them user vao project

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -6,6 +6,15 @@ let CreateAProject = async (data) => {
       let result = await Project.create(data);
       return result;
     }
+    if (data.type === "ADD-USERS") {
+      let result = await Project.findById(data.projectId).exec();
+
+      for (let i = 0; i < data.usersArr.length; i++) {
+        result.usersInfor.push(data.usersArr[i]);
+      }
+      await result.save();
+      return result;
+    }
   } catch (error) {
     console.log("ERROR >>> ", error);
     return null;
